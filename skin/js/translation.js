3deb4a4470279826a8cb0dcdeac32cccd44b0ddd
ReneMuetti <ReneMuetti@googlemail.com>
2023-08-04 14:50:50 +0200

    * auto-replace for " in all translated strings

--- skin/js/translation.js
+++ skin/js/translation.js
@@ -6,37 +6,6 @@ $(document).ready(function() {
     bindEventForTextarea();
 });
 
-function nl2br (str, is_xhtml) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   improved by: Philip Peterson
-  // +   improved by: Onno Marsman
-  // +   improved by: Atli Þór
-  // +   bugfixed by: Onno Marsman
-  // +      input by: Brett Zamir (http://brett-zamir.me)
-  // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   improved by: Brett Zamir (http://brett-zamir.me)
-  // +   improved by: Maximusya
-  // *     example 1: nl2br('Kevin\nvan\nZonneveld');
-  // *     returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
-  // *     example 2: nl2br("\nOne\nTwo\n\nThree\n", false);
-  // *     returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
-  // *     example 3: nl2br("\nOne\nTwo\n\nThree\n", true);
-  // *     returns 3: '<br />\nOne<br />\nTwo<br />\n<br />\nThree<br />\n'
-  var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
-
-  return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
-}
-
-function replaceQuotes(str) {
-    let count = 0;
-
-    return str.replace(/"/g, () => {
-        count++;
-        return count % 2 === 1 ? "«" : "»";
-    });
-}
-
 function getCurrentDivText() {
     let currentDivText = $("#source-string").text();
     //console.log("Current DIV Text:", currentDivText);
@@ -49,14 +18,14 @@ function bindEventForTextarea()
         let textarea = $(this);
 
         setTimeout(function() {
-            let currentDivText = getCurrentDivText();
+            //let currentDivText = getCurrentDivText();
 
-            if ( currentDivText.includes("«") ) {
+            //if ( currentDivText.includes("«") ) {
                 let orgTranslation = textarea.val();
                 let newTranslation = replaceQuotes(orgTranslation);
 
                 textarea.val(newTranslation);
-            }
+            //}
         }, 100);
     });
 }
--- skin/js/translation_details.js
+++ skin/js/translation_details.js
@@ -2,26 +2,22 @@ $(document).ready(function() {
     // currently nothing
 });
 
-function nl2br (str, is_xhtml) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   improved by: Philip Peterson
-  // +   improved by: Onno Marsman
-  // +   improved by: Atli Þór
-  // +   bugfixed by: Onno Marsman
-  // +      input by: Brett Zamir (http://brett-zamir.me)
-  // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   improved by: Brett Zamir (http://brett-zamir.me)
-  // +   improved by: Maximusya
-  // *     example 1: nl2br('Kevin\nvan\nZonneveld');
-  // *     returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
-  // *     example 2: nl2br("\nOne\nTwo\n\nThree\n", false);
-  // *     returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
-  // *     example 3: nl2br("\nOne\nTwo\n\nThree\n", true);
-  // *     returns 3: '<br />\nOne<br />\nTwo<br />\n<br />\nThree<br />\n'
-  var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
+function bindEventForTextarea(senderID)
+{
+    $("#input-" + senderID).on("input", function() {
+        let textarea = $(this);
 
-  return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+        setTimeout(function() {
+            //let currentDivText = getCurrentSourceText("#source-string");
+
+            //if ( currentDivText.includes("«") ) {
+                let orgTranslation = textarea.val();
+                let newTranslation = replaceQuotes(orgTranslation);
+
+                textarea.val(newTranslation);
+            //}
+        }, 100);
+    });
 }
 
 /**
@@ -90,7 +86,7 @@ function changeLanguage()
 }
 
 /**
- * toggle DIV and INPUT in Language-Details-List
+ * toggle DIV and TEXTAREA in Language-Details-List
  * enable Edit-Mode in Detail-List-Per-File
  *
  * @param integer   Translation-ID
@@ -103,6 +99,7 @@ function toggleEditTranslation(senderID)
         $("#translate-" + senderID).hide();
         $("#input-" + senderID).show();
         $("#save-button-" + senderID).show();
+        bindEventForTextarea(senderID);
     }
     else {
         if ( $("#input-" + senderID).val() != $("#translate-" + senderID).html() ) {
@@ -111,7 +108,7 @@ function toggleEditTranslation(senderID)
         }
 
         $("#translate-" + senderID).show();
-        $("#input-" + senderID).hide();
+        $("#input-" + senderID).hide().off("input");
         $("#save-button-" + senderID).hide();
     }
 }
